55acb24accaae6c4d6d76719818d2bf50ff7c7df
Admin1 <Admin1@DESKTOP-D860E6K>
2023-05-26 00:05:39 +0530

    - Added JWT token implementation.

--- src/main/java/com/taskhub/congif/SecurityConfig.java
+++ src/main/java/com/taskhub/congif/SecurityConfig.java
@@ -3,23 +3,42 @@ package com.taskhub.congif;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.taskhub.filters.JwtFilter;
 import com.taskhub.service.UserService;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+	
+	
+	private JwtAuthEntryPoint jwtAuthEntryPoint;
+	private UserService userService;
+	
+	@Autowired
+    public SecurityConfig(UserService userService, JwtAuthEntryPoint jwtAuthEntryPoint) {
+        this.userService = userService;
+        this.jwtAuthEntryPoint = jwtAuthEntryPoint;
+    }
+ 
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
 
-		httpSecurity.csrf().disable().authorizeHttpRequests().requestMatchers("/user/register")
-				.permitAll().anyRequest().authenticated().and().formLogin();
+		httpSecurity.csrf().disable().authorizeHttpRequests().requestMatchers("/user/register", "/authenticate")
+				.permitAll().anyRequest().authenticated().and().exceptionHandling().authenticationEntryPoint(jwtAuthEntryPoint).and().sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
+		httpSecurity.addFilterBefore(jwtFilter(), UsernamePasswordAuthenticationFilter.class);
 		return httpSecurity.build();
 	}
 
@@ -29,11 +48,14 @@ public class SecurityConfig {
 	}
 
 	@Bean
-	public DaoAuthenticationProvider getDaoAuthProvider(UserService userService) {
-	  DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-	  provider.setUserDetailsService(userService);
-	  provider.setPasswordEncoder(new BCryptPasswordEncoder());
-	  return provider;
-	}
+    public AuthenticationManager authenticationManager(
+            AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+	
+	@Bean
+    public JwtFilter jwtFilter() {
+        return new JwtFilter();
+    }
 
 }
--- src/main/java/com/taskhub/service/UserService.java
+++ src/main/java/com/taskhub/service/UserService.java
@@ -1,9 +1,9 @@
 package com.taskhub.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -16,8 +16,9 @@ public class UserService implements UserDetailsService{
 	@Autowired
 	UserRepository userRepository;
 	
-	@Autowired
-	private PasswordEncoder passwordEncoder;
+	private PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+	
+	
 	
 	public void saveUser(User user) {
 		user.setPassword(passwordEncoder.encode(user.getPassword()));
@@ -25,7 +26,7 @@ public class UserService implements UserDetailsService{
 	}
 
 	@Override
-	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+	public User loadUserByUsername(String username) throws UsernameNotFoundException {
 		
 		User user = userRepository.findByUserName(username);
 		
