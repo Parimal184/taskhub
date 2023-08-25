f6a8de96e367fd1e7698f9484076c29471ae8879
Parimal18 <parimaltandel184@gmail.com>
2023-08-25 23:37:52 +0530

    - sort by status query added

--- src/main/java/com/taskhub/repository/TaskRepository.java
+++ src/main/java/com/taskhub/repository/TaskRepository.java
@@ -3,11 +3,16 @@ package com.taskhub.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.taskhub.common.TaskStatus;
 import com.taskhub.model.Task;
 
 public interface TaskRepository extends JpaRepository<Task, Long>{
 	
+	@Query("SELECT t from Task t order by t.updated desc")
+	List<Task> findAll();
+	
+	@Query("SELECT t from Task t where t.status like :status order by t.updated desc")
 	List<Task> findByStatus(TaskStatus status);
 }
